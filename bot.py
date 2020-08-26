a9728961a9826129a84a3b18f3209f85c1e25978
bluo999 <brianluo999@gmail.com>
2020-08-26 00:32:30 -0700

    Added create voice channel, rename channel

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import functools
 import os
 
 import discord
@@ -10,21 +11,52 @@ GUILD = os.getenv('DISCORD_GUILD')
 
 bot = commands.Bot(command_prefix='!')
 
+def restrict_channel(func):
+    @functools.wraps(func)
+    async def wrapper(*args, **kwargs):
+        ctx = args[0]
+        if ctx.channel.name == ctx.author.name:
+            return await func(*args, **kwargs)
+    return wrapper
+
+
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.errors.CheckFailure):
-        await ctx.send('You do not have the correct role for this command.')
+        await ctx.send('You do not have the correct role for this command')
     else:
         print(error)
 
-@bot.command(name='create-channel')
-@commands.has_role('admin')
+
+@bot.command(name='create')
+@commands.has_role('ADMIN')
+@restrict_channel
 async def create_channel(ctx, channel_name):
+    print(f'Creating new channel: {channel_name}')
+    await ctx.guild.create_text_channel(channel_name)
+    await ctx.send(f'Created text channel {channel_name}')
+
+
+@bot.command(name='create-voice')
+@commands.has_role('ADMIN')
+@restrict_channel
+async def create_voice_channel(ctx, channel_name):
+    print(f'Creating new voice channel: {channel_name}')
+    await ctx.guild.create_voice_channel(channel_name)
+    await ctx.send(f'Created voice channel {channel_name}')
+    
+
+@bot.command(name='rename')
+@commands.has_role('ADMIN')
+@restrict_channel
+async def rename_channel(ctx, channel_name, new_channel_name):
     guild = ctx.guild
-    existing_channel = discord.utils.get(guild.channels, name=channel_name)
-    if not existing_channel:
-        print(f'Creating new channel: {channel_name}')
-        await guild.create_text_channel(channel_name)
+    channel = discord.utils.get(guild.channels, name=channel_name)
+    if channel is not None:
+        await channel.edit(name=new_channel_name)
+        await ctx.send(f'Renamed channel {channel_name} to {new_channel_name}')
+    else:
+        await ctx.send(f'Channel {channel_name} does not exist')
 
 
 bot.run(TOKEN)
