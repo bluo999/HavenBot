4fde4eac5a7250c3f99c78baec4101aedbd1428b
Brian Luo <brianluo999@gmail.com>
2020-10-17 01:23:44 -0700

    Added global checks (#9)
    Added global checks for user restrictions, disabled commands, and admin role

--- src/bot.py
+++ src/bot.py
@@ -10,13 +10,54 @@ from discord.ext import commands
 from config import CONFIG
 
 initial_extensions = ['channels', 'messages']
+ADMIN_ID = int(CONFIG['Discord Info']['AdminRoleID'])
+DISABLED_COMMANDS = CONFIG['Restrictions']['DisabledCommands'].split(',')
 
 bot = commands.Bot(command_prefix='!', description='Haven Bot')
 
 
+class DisabledCommandError(commands.CommandError):
+    """Disabled command was attempted to be called."""
+
+    def __str__(self):
+        return 'This command is disabled.'
+
+
+class AdminError(commands.CommandError):
+    """Non-admin tries to use a command."""
+
+    def __str__(self):
+        return 'You do not have permission.'
+
+
+@bot.check
+async def globally_restrict_user(ctx):
+    username = CONFIG['Restrictions']['Username']
+    if username == '' or username == ctx.author.name:
+        return True
+    else:  # Maybe eventually raise specific error
+        return False
+
+
+@bot.check
+async def globally_disabled_commands(ctx):
+    if ctx.command.name in DISABLED_COMMANDS:
+        raise DisabledCommandError()
+
+    return True
+
+
+@bot.check
+async def globally_check_admin(ctx):
+    for r in ctx.message.author.roles[1:]:
+        if r.id == ADMIN_ID:
+            return True
+
+    raise AdminError()
+
+
 @bot.event
 async def on_command_error(ctx, error):
-    """Send the error to Discord chat."""
     logging.error(error)
     await ctx.send(error)
 
--- src/cogs/channels.py
+++ src/cogs/channels.py
@@ -5,8 +5,6 @@ import logging
 from discord import utils
 from discord.ext.commands import Cog, command, has_role
 
-from common import restrict_user
-
 
 def setup(bot):
     """Load the Channel cog."""
@@ -20,24 +18,18 @@ class ChannelCog(Cog, name='Channel'):
         self.bot = bot
 
     @command(name='create-text')
-    @has_role('ADMIN')
-    @restrict_user
     async def create_channel(self, ctx, channel_name):
         """Create a new text channel."""
         await ctx.guild.create_text_channel(channel_name)
         await ctx.send(f'Created text channel {channel_name}')
 
     @command(name='create-voice')
-    @has_role('ADMIN')
-    @restrict_user
     async def create_voice_channel(self, ctx, channel_name):
         """Create a new voice channel."""
         await ctx.guild.create_voice_channel(channel_name)
         await ctx.send(f'Created voice channel {channel_name}')
 
-    @command(name='rename')
-    @has_role('ADMIN')
-    @restrict_user
+    @command(name='rename-channel')
     async def rename_channel(self, ctx, channel_name, new_channel_name):
         """Rename a text or voice channel."""
         channel = utils.get(ctx.guild.channels, name=channel_name)
@@ -48,8 +40,6 @@ class ChannelCog(Cog, name='Channel'):
             await ctx.send(f'Channel {channel_name} does not exist')
 
     @command(name='delete-channel')
-    @has_role('ADMIN')
-    @restrict_user
     async def delete_channel(self, ctx, channel_name):
         """Delete a text or voice channel."""
         channel = utils.get(ctx.guild.channels, name=channel_name)
